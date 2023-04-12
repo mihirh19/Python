0d270e6b0078cc7ebe9ca3118030ddbe2a452ea1
mihirh19 <miheerhadvani990@gmail.com>
2023-04-12 17:53:50 +0530

    😀LeetCode 1773.Count_Items_Matching_a_Rule.py solved

---
+++ LeetCodeSolution/1773.Count_Items_Matching_a_Rule.py
@@ -0,0 +1,61 @@
+"""
+You are given an array items, where each items[i] = [typei, colori, namei] describes the type, color, and name of the ith item. You are also given a rule represented by two strings, ruleKey and ruleValue.
+
+The ith item is said to match the rule if one of the following is true:
+
+ruleKey == "type" and ruleValue == typei.
+ruleKey == "color" and ruleValue == colori.
+ruleKey == "name" and ruleValue == namei.
+Return the number of items that match the given rule.
+
+
+
+Example 1:
+
+Input: items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], ruleKey = "color", ruleValue = "silver"
+Output: 1
+Explanation: There is only one item matching the given rule, which is ["computer","silver","lenovo"].
+Example 2:
+
+Input: items = [["phone","blue","pixel"],["computer","silver","phone"],["phone","gold","iphone"]], ruleKey = "type", ruleValue = "phone"
+Output: 2
+Explanation: There are only two items matching the given rule, which are ["phone","blue","pixel"] and ["phone","gold","iphone"]. Note that the item ["computer","silver","phone"] does not match.
+
+
+Constraints:
+
+1 <= items.length <= 104
+1 <= typei.length, colori.length, namei.length, ruleValue.length <= 10
+ruleKey is equal to either "type", "color", or "name".
+All strings consist only of lowercase letters.
+"""
+from typing import List
+
+
+class Solution:
+    def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+        count = 0
+        for item in items:
+            if ruleKey == "type":
+                if item[0] == ruleValue:
+                    count += 1
+            elif ruleKey == "color":
+                if item[1] == ruleValue:
+                    count += 1
+            elif ruleKey == "name":
+                if item[2] == ruleValue:
+                    count += 1
+
+        return count
+
+
+if __name__ == '__main__':
+    items = [["phone", "blue", "pixel"], ["computer", "silver", "lenovo"], ["phone", "gold", "iphone"]]
+    ruleKey = "color"
+    ruleValue = "silver"
+    print(Solution().countMatches(items, ruleKey, ruleValue))
+
+    items2 = [["phone", "blue", "pixel"], ["computer", "silver", "phone"], ["phone", "gold", "iphone"]]
+    ruleKey2 = "type"
+    ruleValue2 = "phone"
+    print(Solution().countMatches(items2, ruleKey2, ruleValue2))
