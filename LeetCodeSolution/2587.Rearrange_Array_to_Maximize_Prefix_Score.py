16abbd0f3c13d986589ade211e9f2d4648fd8260
mihirh19 <miheerhadvani990@gmail.com>
2023-04-14 14:44:09 +0530

    😀LeetCode 0141,2570, 2586, 2587 solved

---
+++ LeetCodeSolution/2587.Rearrange_Array_to_Maximize_Prefix_Score.py
@@ -0,0 +1,47 @@
+"""
+You are given a 0-indexed integer array nums. You can rearrange the elements of nums to any order (including the given order).
+
+Let prefix be the array containing the prefix sums of nums after rearranging it. In other words, prefix[i] is the sum of the elements from 0 to i in nums after rearranging it. The score of nums is the number of positive integers in the array prefix.
+
+Return the maximum score you can achieve.
+
+
+
+Example 1:
+
+Input: nums = [2,-1,0,1,-3,3,-3]
+Output: 6
+Explanation: We can rearrange the array into nums = [2,3,1,-1,-3,0,-3].
+prefix = [2,5,6,5,2,2,-1], so the score is 6.
+It can be shown that 6 is the maximum score we can obtain.
+Example 2:
+
+Input: nums = [-2,-3,0]
+Output: 0
+Explanation: Any rearrangement of the array will result in a score of 0.
+
+
+Constraints:
+
+1 <= nums.length <= 105
+-106 <= nums[i] <= 106
+"""
+from typing import List
+
+
+class Solution:
+    def maxScore(self, nums: List[int]) -> int:
+        nums.sort(reverse=True)
+        p_sum = [0]
+        for n in nums:
+            p_sum.append(p_sum[-1] + n)
+        m = 0
+        for i in range(1, len(p_sum)):
+            if p_sum[i] > 0:
+                m = i
+        return m
+
+
+if __name__ == '__main__':
+    nums = [2, -1, 0, 1, -3, 3, -3]
+    print(Solution().maxScore(nums))
