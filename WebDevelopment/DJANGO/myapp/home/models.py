77a5881e8955a3f300df7cd04247b622ca7df7cc
mihirh19 <miheerhadvani990@gmail.com>
2023-04-14 14:45:33 +0530

    🕸️Web devlopment django project added

---
+++ WebDevelopment/DJANGO/myapp/home/models.py
@@ -0,0 +1,15 @@
+import email
+from os import name
+from statistics import mode
+from django.db import models
+
+# Create your models here.
+
+class Contact(models.Model):
+   name = models.CharField(max_length=122)
+   email =models.CharField(max_length=120)
+   desc = models.TextField()
+   date = models.DateField()
+
+   def __str__(self):
+      return self.name
---
+++ WebDevelopment/DJANGO/myapp/myapp/views.py
@@ -0,0 +1,8 @@
+# from django.http import HttpResponse
+
+# def index(request):
+#    return HttpResponse("home")
+
+
+# def about(request):
+#    return HttpResponse("about")
