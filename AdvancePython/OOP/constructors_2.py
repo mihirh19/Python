dcb53b4c3ee7ff0c544023d40ed668aae26d4435
mihirh19 <miheerhadvani990@gmail.com>
2023-09-29 11:13:08 +0530

    test cases added for simple stack in python

---
+++ AdvancePython/OOP/constructors_2.py
@@ -0,0 +1,15 @@
+# Example code for constructors in Python
+
+# Define a class
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+
