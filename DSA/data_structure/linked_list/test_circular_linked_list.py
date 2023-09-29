23820275c87747d843652c8431f9b75007149c9c
mihirh19 <miheerhadvani990@gmail.com>
2023-09-29 13:19:32 +0530

    test case fixes added

--- DSA/data_structure/linked_list/test_circular_linked_list.py
+++ DSA/data_structure/linked_list/test_circular_linked_list.py
@@ -1,15 +1,18 @@
+import pytest
 from . import circular_linked_list as cll
 
-l1 = cll.CircularLinkedList()
 
-def test_insert_head():
-   l1.insert_head(1)
-   assert l1.head.data == 1
+@pytest.fixture(scope="module")
+def init_link():
+   return cll.CircularLinkedList()
+def test_insert_head(init_link):
+   init_link.insert_head(1)
+   assert init_link.head.data == 1
 
-def test_insert_tail():
-   l1.insert_tail(2)
-   assert l1.tail.data == 2
+def test_insert_tail(init_link):
+   init_link.insert_tail(2)
+   assert init_link.tail.data == 2
 
-def test_insert_at_index():
-   l1.insert_at_index(1, 3)
-   assert l1.head.next.data == 3
+def test_insert_at_index(init_link):
+   init_link.insert_at_index(1, 3)
+   assert init_link.head.next.data == 3
--- DSA/data_structure/stack/test_simple_stack.py
+++ DSA/data_structure/stack/test_simple_stack.py
@@ -1,20 +1,25 @@
 from . import  Stack
-s = Stack()
-def test___init__():
+import pytest
+
+@pytest.fixture(scope="module")
+def setup_stack():
+   return Stack()
    
-   assert s._capacity == 10
-   assert s._size == 0
-   assert s._items == [None] * 10
+def test___init__(setup_stack):
+   
+   assert setup_stack._capacity == 10
+   assert setup_stack._size == 0
+   assert setup_stack._items == [None] * 10
 
-def test_push():
-   s.push(1)
-   s.push(2)
-   s.push(3)
-   s.push(4)
-   print(s)
-   assert s.peek() == 4
+def test_push(setup_stack):
+   setup_stack.push(1)
+   setup_stack.push(2)
+   setup_stack.push(3)
+   setup_stack.push(4)
+   print(setup_stack)
+   assert setup_stack.peek() == 4
 
-def test_pop():
-   s.pop()
-   assert s.peek() == 3
+def test_pop(setup_stack):
+   setup_stack.pop()
+   assert setup_stack.peek() == 3
 
